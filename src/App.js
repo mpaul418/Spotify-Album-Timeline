b361648c107d531ae10c7afa325b2234c720db12
Michael McGillicuddy <mpm0418@gmail.com>
2022-04-20 00:46:34 -0400

    styling and borders around albums

--- src/App.js
+++ src/App.js
@@ -6,43 +6,62 @@ function App() {
 
   const Timeline = styled('div', {
     display: 'flex',
+    justifyContent: 'center',
+  })
+
+  const AlbumList = styled('div', {
+    display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
   })
 
   const Album = styled('div', {
-    // width: 600,
-    // height: 600,
-    // TODO 
+    display: 'flex',
+    flexDirection: 'row',
+    border: '1px solid black',
+    padding: '10px',
+    margin: '10px 0',
+  })
+
+  const AlbumInfo = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    paddingTop: 16,
   })
 
   const AlbumCover = styled('img', {
     width: 150,
     height: 150,
-    // TODO 
+    paddingRight: 24,
   })
 
   // TODO need to do lots of null checking on fields that may/may not exist??
   // TODO add more styling/etc
   const renderItems = () => {
     return sampleData.items.map(item => {
-      const { added_at: addedDate, album } = item;
+      const { added_at, album } = item;
+      const addedDate = added_at.split('T')[0];
       return (<Album key={album.id}>
-        <p>{album.name}</p>
-        <p>{album.artists[0].name /* TODO need to parse through all the artists */}</p>
-        <p>Released on: {album.release_date}</p>
-        <p>Added on: {addedDate}</p>
         <a href={album.external_urls.spotify} target="_blank" rel="noreferrer">
-          <AlbumCover src={album.images[1].url /* TODO need to have this parse the correct image */} />
+          <AlbumCover src={album.images[1].url /* TODO need to have this parse the correctly sized image */} />
         </a>
+        <AlbumInfo>
+          <span>{album.name}</span>
+          <span>{album.artists[0].name /* TODO need to parse through all the artists */}</span>
+          <span>Released on: {album.release_date}</span>
+          <span>Added on: {addedDate}</span>
+        </AlbumInfo>
+
       </Album>)
     })
   }
 
   return (
     <Timeline>
-      {renderItems()}
-    </Timeline >
+      <AlbumList>
+        {renderItems()}
+      </AlbumList>
+    </Timeline>
   );
 }
 
