072a013d80e9f6bfa936594eba37c2ba57dc9d84
Michael McGillicuddy <mpm0418@gmail.com>
2022-12-16 01:59:10 -0500

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -7,7 +7,9 @@ import Cookies from 'js-cookie';
 import 'react-spotify-auth/dist/index.css';
 
 export const App = () => {
-  const redirectUri = process.env.NODE_ENV === 'production' ? 'https://urchin-app-56urh.ondigitalocean.app/' : 'http://localhost:3000/'; // TODO change prod url
+  const redirectUri =
+    process.env.NODE_ENV === 'production' ? 'https://urchin-app-56urh.ondigitalocean.app/' : 'http://localhost:3000/'; // TODO change prod url
+  console.log('env: ', process.env);
   const [spotifyAuthToken, setSpotifyAuthToken] = useState();
 
   useEffect(() => {
@@ -42,6 +44,7 @@ export const App = () => {
         </p>
       </HomePageText>
       <SpotifyAuth
+        // TODO figure out how to not have access token in URL
         redirectUri={redirectUri}
         clientID='454b032f839c4ce7adccd951bcd5163f'
         scopes={[Scopes.userLibraryRead]}
