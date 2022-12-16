d0c068177ac82473208d6c2c572196e54b608d4e
Michael McGillicuddy <mpm0418@gmail.com>
2022-12-16 02:05:10 -0500

    try env again

--- src/App/App.js
+++ src/App/App.js
@@ -7,9 +7,7 @@ import Cookies from 'js-cookie';
 import 'react-spotify-auth/dist/index.css';
 
 export const App = () => {
-  const redirectUri =
-    process.env.NODE_ENV === 'production' ? 'https://urchin-app-56urh.ondigitalocean.app/' : 'http://localhost:3000/'; // TODO change prod url
-  console.log('env: ', process.env);
+  const redirectUri = process.env.NODE_ENV === 'production' ? process.env.REDIRECT_URI : 'http://localhost:3000/';
   const [spotifyAuthToken, setSpotifyAuthToken] = useState();
 
   useEffect(() => {
