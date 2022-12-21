d441876bbfe6a9e08824e42fe52dbb276d138dac
Michael McGillicuddy <mpm0418@gmail.com>
2022-12-20 21:23:34 -0500

    add github link to bottom of homepage

--- src/App/App.js
+++ src/App/App.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { SpotifyApiContext, UserAlbums } from 'react-spotify-api';
 import { SpotifyAuth, Scopes } from 'react-spotify-auth';
 import { Timeline } from '../Timeline';
-import { HomePageContent, HomePageText, LogoutButton } from './styles';
+import { HomePageContent, HomePageText, Footer, FooterImage, FooterButton, LogoutButton } from './styles';
 import Cookies from 'js-cookie';
 import 'react-spotify-auth/dist/index.css';
+import githubLogo from './github-mark-white.png';
 
 export const App = () => {
   const [spotifyAuthToken, setSpotifyAuthToken] = useState();
@@ -33,22 +34,34 @@ export const App = () => {
   );
 
   const HomePage = () => (
-    <HomePageContent>
-      <HomePageText>
-        <p>
-          <b>Spotify Album Timeline</b>
-        </p>
-        <p style={{ marginTop: 0 }}>
-          Login to see a timeline of all the albums you have saved to Spotify, including both the album's release date
-          and the date that you added it.
-        </p>
-      </HomePageText>
-      <SpotifyAuth
-        redirectUri={window.location.href}
-        clientID='454b032f839c4ce7adccd951bcd5163f'
-        scopes={[Scopes.userLibraryRead]}
-      />
-    </HomePageContent>
+    <div>
+      <HomePageContent>
+        <HomePageText>
+          <p>
+            <b>Spotify Album Timeline</b>
+          </p>
+          <p style={{ marginTop: 0 }}>
+            Login to see a timeline of all the albums you have saved to Spotify, including both the album's release date
+            and the date that you added it.
+          </p>
+        </HomePageText>
+        <SpotifyAuth
+          redirectUri={window.location.href}
+          clientID='454b032f839c4ce7adccd951bcd5163f'
+          scopes={[Scopes.userLibraryRead]}
+        />
+      </HomePageContent>
+      <Footer>
+        <FooterButton
+          href='https://github.com/mpaul418/Spotify-Album-Timeline'
+          target='_blank'
+          rel='noopener noreferrer' /*onClick={() => window.open('https://github.com/mpaul418/Spotify-Album-Timeline', '_blank')}*/
+        >
+          <FooterImage src={githubLogo} alt='' />
+          GitHub
+        </FooterButton>
+      </Footer>
+    </div>
   );
 
   return spotifyAuthToken ? <TimelineDisplay /> : <HomePage />;
--- src/App/styles.js
+++ src/App/styles.js
@@ -18,6 +18,43 @@ export const HomePageText = styled('div', {
   minWidth: 130,
 });
 
+export const Footer = styled('div', {
+  position: 'fixed',
+  display: 'flex',
+  justifyContent: 'center',
+  bottom: 0,
+  width: '100%',
+  paddingBottom: 15,
+});
+
+export const FooterButton = styled('a', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontFamily: 'Gotham, Arial, Helvetica, sans-serif',
+  padding: '0.25rem 0.5rem',
+  cursor: 'pointer',
+  transitionDuration: '300ms',
+  backgroundColor: '#1A1A1A',
+  color: '#FFFFFF',
+  minHeight: 32,
+  border: 'none',
+  borderRadius: '0.2rem',
+  textDecoration: 'none !important',
+
+  '&:hover': {
+    transitionDuration: '500ms',
+    transform: 'translate(-1px, 2px)',
+    boxShadow: '0 0.25rem 0.75rem 0 #777777',
+  },
+});
+
+export const FooterImage = styled('img', {
+  height: 26,
+  width: 26,
+  paddingRight: 5,
+});
+
 export const LogoutButton = styled('button', {
   padding: 5,
   margin: 10,
